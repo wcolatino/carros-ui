fbbb1ede2423df6e03a8de5571ca3146ff7c8d55
Willie Colatino <wcolatino@hotmail.com>
2025-08-09 21:09:36 -0300

    Criada a tela de login

--- src/app/components/layout/login/login.component.ts
+++ src/app/components/layout/login/login.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { Router } from '@angular/router';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MdbFormsModule],
+  imports: [MdbFormsModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  usuario!: string;
+  senha!: string;
+
+  router = inject(Router); //Equivalente ao @Autowired
+
+  logar(){
+    if(this.usuario == 'admin' && this.senha){
+      this.router.navigate(['admin/carros'])
+    } else {
+      alert('Usuário ou senha incoretos!');
+    }
+  }
 
 }
